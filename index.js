88e7ac9289f8a2e2a275989b44b81abff2877e3b
Janyu <54945627+Janyu427@users.noreply.github.com>
2025-03-06 15:54:15 +0800

    edit PORT value

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
